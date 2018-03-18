38c5cd46d99691fcd163f9f3faa44c21ca08eeed
MaheshBabu Lingutla <lmaheshchowdary@gmail.com>
2018-03-17 22:09:39 -0500

    Add project files.

---
+++ MM_Management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MM_Management.Startup))]
+namespace MM_Management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
